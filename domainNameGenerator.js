11fdf7e0a16e29bc47024a7c2545e8d84a70419b
Heandy27 <122308561+Heandy27@users.noreply.github.com>
2024-03-14 02:15:37 +0000

    [Actualización de método extensión .com, .net, us]

--- domainNameGenerator.js
+++ domainNameGenerator.js
@@ -1,13 +1,25 @@
+/* eslint-disable no-console */
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 let extension = ['.com', '.net', '.us', '.io'];
-let randomExtension = Math.floor(Math.random() * 3) + 1;
+
+
+
 
 for (let p in pronoun) {
     for (let a in adj) {
-        for (let n in noun) {
-            console.log(pronoun[p]+adj[a]+noun[n]+extension[randomExtension]);
+         for (let n in noun) {
+         // console.log(pronoun[p]+adj[a]+noun[n]+extension[Math.floor(Math.random() * 3) + 1]);
+        }
+    }
+}
+
+
+for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+            console.log(pronoun[p]+adj[a]+noun[n]+extension[Math.floor(Math.random() * 3) + 1]);
         }
     }
 }
